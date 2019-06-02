aee4129c7ef00c0f48e704620fd898c2e31b69bc
mingren.chen16 <mingren.chen16@gmail.com>
2019-06-02 16:13:42 +0800

    finish addming validator

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,4 +1,5 @@
 class PhotosController < ApplicationController
+  before_action :set_photo, :only => [:show, :edit, :update, :destroy]
   def index
     @photos = Photo.all
   end
@@ -9,17 +10,40 @@ class PhotosController < ApplicationController
 
   def create
     @photo = Photo.new(photo_params)  #params以拿到使用者從表單傳資的資料
-    @photo.save
+    if @photo.save
+        redirect_to photos_url
+    else
+      render :action => :new
+    end  
 
-    redirect_to photos_url
   end
   # 本create方法須放在private的上面
 
   def show
-    @photo = Photo.find(params[:id])
+  end
+
+  def edit
+  end
+
+  def update
+    if @photo.update_attributes(photo_params)
+      redirect_to photo_path(@photo)
+    else
+        render :action => :edit
+    end
+  end
+
+  def destroy
+    @photo.destroy
+
+    redirect_to photos_url
   end
 
   private
+
+  def set_photo
+    @photo = Photo.find(params[:id])
+  end
 
   def photo_params
     params.require(:photo).permit(:title, :date, :description, :file_location)
--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,2 +1,4 @@
 class Photo < ApplicationRecord
+  # mount_uploarder:file_location , PhotoImageUploader
+  validates_presence_of :title, :date, :description, :file_location
 end
